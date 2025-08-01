693c9282f5bb5edbd0861f85b80b6aa22e868b77
O2xygeno2 <o2xygeno2@gmail.com>
2025-08-01 15:46:48 +0530

    Create db.py

---
+++ fastapi/app/app/db.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}"
+    f"@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
