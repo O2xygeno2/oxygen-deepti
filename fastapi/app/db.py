625cf506c53ad073b7777b55721c21aa38227755
O2xygeno2 <o2xygeno2@gmail.com>
2025-08-25 20:08:22 +0530

    Update db.py

--- fastapi/app/db.py
+++ fastapi/app/db.py
@@ -9,6 +9,7 @@ DATABASE_URL = (
     f"postgresql+asyncpg://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}"
     f"@/{os.getenv('DATABASE_NAME')}?host={os.getenv('DATABASE_HOST')}"
 )
+postgresql+asyncpg://deepti-db:DeeptiGarg0111!@/appdb?host=/cloudsql/master-shell-468709-v8:asia-south1:oxygen-db-instance
 
 # Create async engine
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
